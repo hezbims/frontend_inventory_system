c5e93517f971a6894d148cc7ec659973b91d7e6d
hezbims <hezbisulaiman@gmail.com>
2023-09-22 11:18:40 +0800

    modifikasi url biar kompatibel ke mobile

--- common/lib/constant/url/common_url.dart
+++ common/lib/constant/url/common_url.dart
@@ -1,19 +1,21 @@
 
+import 'package:flutter/foundation.dart';
+
 abstract class CommonUrl {
   static const _debugApiUrl = 'http://127.0.0.1:8000/api';
   static const _productionApiUrl = 'http://192.168.27.100:9999/api';
   static String get baseApiUrl{
-    // if (!kReleaseMode){
-    //   return _debugApiUrl;
-    // }
+    if (!kReleaseMode && kIsWeb){
+      return _debugApiUrl;
+    }
     return _productionApiUrl;
   }
 
   // TODO : Ganti sama env pusher_app_key
   static String get webSocketUrl {
-    // if (!kReleaseMode){
-    //   return 'ws://127.0.0.1:6001/app/0034c6855cd02144d9f7';
-    // }
+    if (!kReleaseMode && kIsWeb){
+      return 'ws://127.0.0.1:6001/app/0034c6855cd02144d9f7';
+    }
     return 'ws://192.168.27.100:6001/app/0034c6855cd02144d9f7';
   }
 
