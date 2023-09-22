8eb3564008bdd23ee07eba7b576a99193bab772d
hezbims <hezbisulaiman@gmail.com>
2023-09-22 10:05:57 +0800

    fix layout tahun bulan laporan di mobile

--- fitur/fitur_buat_laporan/lib/presentation/page/pilih_bulan_tahun_page.dart
+++ fitur/fitur_buat_laporan/lib/presentation/page/pilih_bulan_tahun_page.dart
@@ -1,6 +1,7 @@
+import 'package:common/domain/extension/media_query_data_extension.dart';
 import 'package:common/presentation/bottom_navbar/stock_bottom_navbar.dart';
+import 'package:common/presentation/button/disabled_submit_button.dart';
 import 'package:common/presentation/button/submit_button.dart';
-import 'package:common/presentation/constraint/constrained_width.dart';
 import 'package:common/presentation/textfield/custom_dropdown_menu.dart';
 import 'package:common/presentation/textfield/custom_textfield.dart';
 import 'package:common/presentation/textfield/style/spacing.dart';
@@ -9,6 +10,7 @@ import 'package:dependencies/provider.dart';
 import 'package:fitur_buat_laporan/data/repository/get_data_laporan_repository_impl.dart';
 import 'package:fitur_buat_laporan/domain/model/month.dart';
 import 'package:fitur_buat_laporan/presentation/provider/pilih_bulan_tahun_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class PilihTahunBulanPage extends StatelessWidget {
@@ -37,7 +39,10 @@ class PilihTahunBulanPage extends StatelessWidget {
                 }
               );
             }
-            return ConstrainedWidthCenter(
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).maxHorizontalPadding + 24
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -81,10 +86,12 @@ class PilihTahunBulanPage extends StatelessWidget {
 
                   SizedBox(
                     width: double.infinity,
-                    child: SubmitButton(
-                      label: "Download CSV",
-                      onTap: provider.downloadCSV
-                    ),
+                    child: kIsWeb ?
+                      SubmitButton(
+                        label: "Download CSV",
+                        onTap: provider.downloadCSV
+                      ) :
+                      const DisabledSubmitButton(label: 'Download CSV',),
                   ),
 
                 ],
