f8689982ebc10a1a42f405c7a19592fa3406b56d
hezbims <hezbisulaiman@gmail.com>
2023-09-29 16:37:31 +0800

    update env interface agar full abstract

--- common/lib/constant/url/env/i_env.dart
+++ common/lib/constant/url/env/i_env.dart
@@ -1,7 +1,6 @@
 abstract class IEnv {
   String get serverHost;
   String get serverPort;
-  final String pusherAppKey = '0034c6855cd02144d9f7';
-  final String websocketPort = '6001';
-
+  String get pusherAppKey;
+  String get  websocketPort;
 }
