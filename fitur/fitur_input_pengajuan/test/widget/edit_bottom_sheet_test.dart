9ae1fd3760a89c0356e30e2fe38fecbb361a2a11
hezbims <hezbisulaiman@gmail.com>
2023-10-04 15:29:38 +0800

    Commit sementara karena bakal ada refactor gede
    - bakal ada refactor gede yang ngelibatin get it lifecycle. Nanti
    setiap dependency tetep ada lifecyclenya. tapi dependencynya disetup
    semua pas di awal pake register factory. Nanti kalo pas test.
    implementasinya bisa di swap

--- fitur/fitur_input_pengajuan/test/widget/edit_bottom_sheet_test.dart
+++ fitur/fitur_input_pengajuan/test/widget/edit_bottom_sheet_test.dart
@@ -10,6 +10,7 @@ import 'package:fitur_input_pengajuan/domain/model/pengajuan.dart';
 import 'package:fitur_input_pengajuan/presentation/component/pilih_barang/barang_quantity_incrementer.dart';
 import 'package:fitur_input_pengajuan/presentation/pages/main_form.dart';
 import 'package:fitur_input_pengajuan/presentation/provider/main_form/main_form_provider.dart';
+import 'package:fitur_input_pengajuan/presentation/provider/pilih_barang/bottom_sheet_barang_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -66,7 +67,7 @@ void main(){
       await tester.pumpWidget(siapkanMainFormDenganDuaBarangAjuan());
 
       await tester.ensureVisible(find.byIcon(Icons.delete).last);
-      await tester.pumpAndSettle();
+      await tester.pump();
       await tester.tap(
           find.byIcon(Icons.delete).last,
       );
@@ -99,6 +100,34 @@ void main(){
 
       expect(find.text('Keterangan : ket-1'), findsOneWidget);
       expect(find.text('Quantity : 9'), findsOneWidget);
+    }
+  );
+
+  testWidgets(
+    'Diberikan main form dengan dua barang ajuan '
+    'Ketika user mencoba mengedit dengan quantity yang tidak valid '
+    'Maka error akan terdisplay di bawah field quantity ',
+    (tester) async {
+      await tester.pumpWidget(siapkanMainFormDenganDuaBarangAjuan());
+
+      await tester.tap(find.byIcon(Icons.edit).first);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(
+        find.descendant(
+          of: find.byType(BarangQuantityIncrementer),
+          matching: find.byType(TextField)
+        ),
+        'xyz'
+      );
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      final provider = GetIt.I.get<BottomSheetBarangProvider>();
+      expect(
+        find.widgetWithText(BarangQuantityIncrementer , provider.quantityError!),
+        findsOneWidget
+      );
     }
   );
 }
