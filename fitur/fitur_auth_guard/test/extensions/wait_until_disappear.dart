8cc68b068a2322812211ea9614f400b180c899c6
hezbims <hezbisulaiman@gmail.com>
2023-10-10 09:06:04 +0800

    mau eksperimen pake go router

---
+++ fitur/fitur_auth_guard/test/extensions/wait_until_disappear.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+extension WidgetTesterWaitUntilDisappear on WidgetTester {
+  Future<void> waitUntilDisappear(
+    Finder finder,{
+    Duration timeout = const Duration(seconds: 20),
+  }) async {
+    final end = binding.clock.now().add(timeout);
+
+    do {
+      if (binding.clock.now().isAfter(end)) {
+        throw Exception('Timed out waiting for $finder');
+      }
+      await pumpAndSettle();
+      await Future.delayed(const Duration(milliseconds: 100));
+
+    } while (finder.evaluate().length == 1);
+  }
+}
--- fitur/fitur_auth_guard/test/unit/auth_guard_unit_test.dart
+++ fitur/fitur_auth_guard/test/unit/auth_guard_unit_test.dart
@@ -1,4 +1,39 @@
-void main(){
 
+
+import 'package:common/constant/test_tags/test_tags.dart';
+import 'package:common/data/repository/token_manager.dart';
+import 'package:common/domain/repository/i_token_manager.dart';
+import 'package:common/presentation/button/submit_button.dart';
+import 'package:dependencies/get_it.dart';
+import 'package:fitur_auth_guard/dependency_injection/fitur_auth_guard_di.dart';
+import 'package:fitur_auth_guard/presentation/page/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../extensions/wait_until_disappear.dart';
+
+void main(){
+  testWidgets(
+    'Ketika passwordname dan username yang dimasukkan tidak cocok, '
+    'Maka pesan error akan terdisplay',
+    (tester) async {
+      GetIt.I.registerSingleton<ITokenManager>(TokenManagerImpl());
+      fiturAuthGuardDi();
+      tester.pumpWidget(
+        MaterialApp(
+          home: const LoginScreen(),
+        ),
+      );
+
+      await tester.tap(find.byType(SubmitButton));
+      await tester.waitUntilDisappear(
+        find.descendant(
+          of: find.byType(LoginScreen),
+          matching: find.byType(CircularProgressIndicator)),
+      );
+      expect(find.text(text), findsOneWidget);
+    },
+    tags: [TestTags.fastTest],
+  );
 }
 
