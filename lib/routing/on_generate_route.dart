d9f2825a2840fb78a8d18f9c7b4ef347b6ecbd33
hezbims <hezbisulaiman@gmail.com>
2023-10-11 11:57:08 +0800

    depth 1 berhasil

--- lib/routing/on_generate_route.dart
+++ lib/routing/on_generate_route.dart
@@ -3,6 +3,7 @@ import 'package:common/constant/routes/routes_path.dart';
 import 'package:fitur_auth_guard/presentation/page/login_screen.dart';
 import 'package:fitur_auth_guard/presentation/route_guard.dart';
 import 'package:fitur_buat_laporan/presentation/page/pilih_bulan_tahun_page.dart';
+import 'package:fitur_lihat_pengajuan/presentation/pages/lihat_pengajuan_pages.dart';
 import 'package:fitur_lihat_stock_barang/presentation/page/lihat_stock_barang_page.dart';
 import 'package:fitur_setting_akun/dependency_setup/setting_akun_page_dependency_setup.dart';
 import 'package:flutter/material.dart';
@@ -33,6 +34,9 @@ Route<dynamic>? onGenerateRoute(RouteSettings settings){
         break;
       case RoutesName.buatLaporanName:
         nextPage = const PilihTahunBulanPage();
+        break;
+      case RoutesName.lihatPengajuanName:
+        nextPage = const LihatPengajuanPage();
         break;
       case RoutesName.settingAkunName:
         nextPage = SettingAkunPageDependencySetup();
