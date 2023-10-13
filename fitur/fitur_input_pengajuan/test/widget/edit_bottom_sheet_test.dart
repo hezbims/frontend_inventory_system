0fe9d708f4ce53f57aca3d54c1d72910cf44c30a
hezbims <hezbisulaiman@gmail.com>
2023-10-13 11:52:43 +0800

    berhasil ngerefactor test case dari edit bottom sheet

--- fitur/fitur_input_pengajuan/test/widget/edit_bottom_sheet_test.dart
+++ fitur/fitur_input_pengajuan/test/widget/edit_bottom_sheet_test.dart
@@ -3,14 +3,15 @@ import 'package:common/constant/test_tags/test_tags.dart';
 import 'package:common/domain/model/pengaju.dart';
 import 'package:common/domain/model/user.dart';
 import 'package:common/presentation/button/cancel_button.dart';
-import 'package:common/presentation/dependency_setup/get_it_dependency_setup.dart';
 import 'package:common/presentation/textfield/custom_textfield.dart';
 import 'package:dependencies/get_it.dart';
 import 'package:fitur_input_pengajuan/domain/model/barang_transaksi.dart';
 import 'package:fitur_input_pengajuan/domain/model/pengajuan.dart';
+import 'package:fitur_input_pengajuan/domain/repository/i_submit_pengajuan_repository.dart';
 import 'package:fitur_input_pengajuan/presentation/component/pilih_barang/barang_quantity_incrementer.dart';
 import 'package:fitur_input_pengajuan/presentation/pages/main_form.dart';
 import 'package:fitur_input_pengajuan/presentation/provider/main_form/main_form_provider.dart';
+import 'package:fitur_input_pengajuan/presentation/provider/pilih_barang/bottom_sheet_barang_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -19,6 +20,26 @@ import '../mock/mock_submit_pengajuan_repository.dart';
 void main(){
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  setUp((){
+    GetIt.I.reset();
+    GetIt.I.registerSingleton(
+        User(token: '', username: 'bukan-admin', isAdmin: false, id: 1)
+    );
+    GetIt.I.registerFactory<ISubmitPengajuanRepository>(
+          () => MockSubmitPengajuanRepository(),
+    );
+
+    GetIt.I.registerFactory(() => BottomSheetBarangProvider());
+
+    GetIt.I.registerFactoryParam(
+          (Pengajuan? initialData , _) => MainFormProvider(
+        user: GetIt.I.get(),
+        initialData: initialData,
+        repository: GetIt.I.get(),
+      ),
+    );
+
+  });
   // Men-test proses edit
   testWidgets(
     'diberikan sebuah main form dengan 2 buah barang ajuan '
@@ -132,39 +153,19 @@ void main(){
 }
 
 Widget siapkanMainFormDenganDuaBarangAjuan(){
-  return MaterialApp(
-    home: Directionality(
-      textDirection: TextDirection.ltr,
-      child: MockMainFormDependencySetup(),
-    ),
-  );
-}
 
-class MockMainFormDependencySetup extends GetItDependencySetup{
-  MockMainFormDependencySetup({
-    super.key,
-  }) : super(
-    setup: (){
-      GetIt.I.registerSingleton(
-        MainFormProvider(
-          user: User(token: '', username: 'bukan-admin', isAdmin: false, id: 1),
-          initialData: Pengajuan(
-            id: 1,
-            tanggal: DateTime(2023),
-            pengaju: Pengaju(nama: 'budi', id: 1, isPemasok: false),
-            status: StatusPengajuan.menunggu,
-            listBarangTransaksi: [
-              BarangTransaksi(id: 1, idBarang: 1, namaBarang: 'barang-1', quantity: 9, keterangan: 'ket-1'),
-              BarangTransaksi(id: 2, idBarang: 2, namaBarang: 'barang-2', quantity: 100, keterangan: 'ket-2'),
-            ],
-          ),
-          repository: MockSubmitPengajuanRepository()
-        ),
-      );
-    },
-    disposeFunction: (){
-      GetIt.I.unregister<MainFormProvider>();
-    },
-    page: MainForm(),
+  final satuPengajuan = Pengajuan(
+    id: 1,
+    tanggal: DateTime(2023),
+    pengaju: Pengaju(nama: 'budi', id: 1, isPemasok: false),
+    status: StatusPengajuan.menunggu,
+    listBarangTransaksi: [
+      BarangTransaksi(id: 1, idBarang: 1, namaBarang: 'barang-1', quantity: 9, keterangan: 'ket-1'),
+      BarangTransaksi(id: 2, idBarang: 2, namaBarang: 'barang-2', quantity: 100, keterangan: 'ket-2'),
+    ],
+  );
+
+  return MaterialApp(
+    home: MainForm(initialData: satuPengajuan),
   );
 }
