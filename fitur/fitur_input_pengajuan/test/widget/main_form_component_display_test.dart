04675c04fcec1c64b20ca73b0a1f110572994672
hezbims <hezbisulaiman@gmail.com>
2023-10-13 12:01:55 +0800

    betulin test case di main form component display

--- fitur/fitur_input_pengajuan/test/widget/main_form_component_display_test.dart
+++ fitur/fitur_input_pengajuan/test/widget/main_form_component_display_test.dart
@@ -2,7 +2,6 @@ import 'package:common/constant/test_tags/test_tags.dart';
 import 'package:common/domain/model/user.dart';
 import 'package:common/presentation/textfield/custom_dropdown_menu.dart';
 import 'package:dependencies/get_it.dart';
-import 'package:dependencies/provider.dart';
 import 'package:fitur_input_pengajuan/domain/model/barang_transaksi.dart';
 import 'package:fitur_input_pengajuan/domain/model/pengajuan.dart';
 import 'package:fitur_input_pengajuan/presentation/component/common/transaksi_barang_bottom_sheet.dart';
@@ -26,7 +25,8 @@ void main() {
         keterangan: ''
     );
   });
-  testWidgets('diberikan sebuah main form dalam kondisi pembuatan pengajuan baru, '
+  testWidgets(
+      'diberikan sebuah main form dalam kondisi pembuatan pengajuan baru, '
       'ketika belum melakukan apa-apa, '
       'maka field grup/pemasok dan field pengeditan list barang tidak akan tampil'
       'ketika tipe pengaju diubah menjadi pemasok, '
@@ -99,26 +99,28 @@ void main() {
       'ketika bottom sheet pertama kali dibuka, maka need focus, '
       'namun kalo pindah ke field keterangan, maka focusnya tetep di keterangan ' , (tester) async {
 
-    final bottomSheetProvider = BottomSheetBarangProvider();
+
+    final provider = BottomSheetBarangProvider();
+    GetIt.I.registerSingleton(provider);
+
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
           body: Directionality(
             textDirection: TextDirection.ltr,
-            child: ChangeNotifierProvider.value(
-              value: bottomSheetProvider,
-              child: TransaksiBarangBottomSheet(initialBarangTransaksi: dummyBarangTransaksi)
+            child: TransaksiBarangBottomSheet(
+              initialBarangTransaksi: dummyBarangTransaksi
             ),
           ),
         ),
       ),
     );
 
-    expect(bottomSheetProvider.quantityFocusNode.hasFocus, isTrue);
+    expect(provider.quantityFocusNode.hasFocus, isTrue);
 
     final keteranganField = find.byType(TextField).last;
     await tester.tap(keteranganField);
-    expect(bottomSheetProvider.quantityFocusNode.hasFocus, isFalse);
+    expect(provider.quantityFocusNode.hasFocus, isFalse);
   } , tags: [TestTags.fastTest]);
 }
 
