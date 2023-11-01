3273635f12987e31a413ba004b6bc4c0c7c0a8e2
hezbims <hezbisulaiman@gmail.com>
2023-11-01 11:13:04 +0800

    integration test untuk chromedriver flutter web
    ngemodifikasi python script agar bisa jalanin integration test
    pada flutter web dengan chromedriver

--- test_script/end_to_end_test.py
+++ test_script/end_to_end_test.py
@@ -4,5 +4,7 @@ os.system(
     "flutter drive "
     "--driver=test_driver/integration_test_driver.dart "
     "--target integration_test\end_to_end_test\end_to_end_test.dart "
-    "-d 063e908a0411"
+    "-d web-server "
+    "--browser-name chrome "
+    "--no-headless "
 )
