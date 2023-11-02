3f2a5558aee9a39eccfb63343f45cd8364454245
hezbims <hezbisulaiman@gmail.com>
2023-11-02 11:50:48 +0800

    sementara commit env

---
+++ common/lib/constant/url/env/release_env.dart
@@ -0,0 +1,17 @@
+import 'package:common/constant/url/env/i_env.dart';
+
+class ReleaseEnv implements IEnv {
+  @override
+  // tag_python_script_ganti_current_ip
+  String get serverHost => '192.168.5.11';
+
+  @override
+  // tag_python_script_ganti_dengan_laravel_port
+  String get serverPort => '9999';
+
+  @override
+  String get pusherAppKey => '0034c6855cd02144d9f7';
+
+  @override
+  String get websocketPort => '6001';
+}
