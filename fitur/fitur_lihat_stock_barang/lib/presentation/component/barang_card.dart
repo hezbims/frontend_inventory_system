346a5e77192fc59b91eac0de591120b42c5fc692
hezbims <hezbisulaiman@gmail.com>
2023-11-08 13:28:43 +0800

    ngehilangin stack di stock barang card

--- fitur/fitur_lihat_stock_barang/lib/presentation/component/barang_card.dart
+++ fitur/fitur_lihat_stock_barang/lib/presentation/component/barang_card.dart
@@ -12,100 +12,90 @@ class BarangCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(4 , 4),
-                  blurRadius: 15,
-                  spreadRadius: -3,
-                  color: Color(0x1a000000),
-                )
-              ],
-            ),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        color: getColorByCurrentStockAndMinStock(
+          minStock: barang.minStock,
+          currentStock: barang.stockSekarang,
         ),
-        Container(
-          decoration: BoxDecoration(
-            color: getColorByCurrentStockAndMinStock(
-              minStock: barang.minStock,
-              currentStock: barang.stockSekarang,
-            ),
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(width: 0.2),
-          ),
+        boxShadow: const [
+          BoxShadow(
+            offset: Offset(4 , 4),
+            blurRadius: 15,
+            spreadRadius: -3,
+            color: Color(0x1a000000),
+          )
+        ],
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(width: 0.2),
+      ),
 
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              barang.nama ,
+              style: TextStyle(
+                fontWeight: CustomFontWeight.medium,
+                fontSize: 18,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  barang.nama ,
-                  style: TextStyle(
-                    fontWeight: CustomFontWeight.medium,
-                    fontSize: 18,
-                  ),
-                ),
+                Expanded(child: Text("Kode : ${barang.kodeBarang}"),),
 
-                const SizedBox(height: 10),
+                const SizedBox(width: 4,),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(child: Text("Kode : ${barang.kodeBarang}"),),
-
-                    const SizedBox(width: 4,),
-
-                    Text("Current stock : ${barang.stockSekarang}"),
-                  ],
-                ),
-
-                // Text(barang.id.toString()),
-
-                const SizedBox(height: 24,),
-
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Text("Kategori :\n${barang.kategori.nama}")
-                    ),
-                    const SizedBox(width: 4,),
-                    Expanded(
-                      flex: 2,
-                      child: Text(
-                        "Rak :\n"
-                        "R${barang.nomorRak}-"
-                        "${barang.nomorLaci}-"
-                        "${barang.nomorKolom}"
-                      )
-                    ),
-                    const SizedBox(width: 4,),
-                    Expanded(
-                      flex: 2,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed(
-                            RoutesPath.inputDataBarangPath(barang.id),
-                          );
-                        },
-                        child: const Text("Edit")
-                      ),
-                    ),
-                  ],
-                )
+                Text("Current stock : ${barang.stockSekarang}"),
               ],
             ),
-          ),
+
+            // Text(barang.id.toString()),
+
+            const SizedBox(height: 24,),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Text("Kategori :\n${barang.kategori.nama}")
+                ),
+                const SizedBox(width: 4,),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    "Rak :\n"
+                    "R${barang.nomorRak}-"
+                    "${barang.nomorLaci}-"
+                    "${barang.nomorKolom}"
+                  )
+                ),
+                const SizedBox(width: 4,),
+                Expanded(
+                  flex: 2,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        RoutesPath.inputDataBarangPath(barang.id),
+                      );
+                    },
+                    child: const Text("Edit")
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
-      ],
+      ),
     );
   }
 
