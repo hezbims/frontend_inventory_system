d1a8dcd10e6f480846ec255aba30635ca4f40336
hezbims <hezbisulaiman@gmail.com>
2023-12-19 09:42:13 +0800

    ngebetulin bug websocket client yang enggak mau ngirim jwt karena salah kodingan di pengecekan isDisposed

--- fitur/fitur_lihat_pengajuan/lib/data/api_client/web_socket_client.dart
+++ fitur/fitur_lihat_pengajuan/lib/data/api_client/web_socket_client.dart
@@ -4,51 +4,58 @@ import 'package:common/constant/url/common_url.dart';
 import 'package:common/domain/model/user.dart';
 import 'package:dependencies/get_it.dart';
 import 'package:dependencies/web_socket_channel.dart';
-import 'package:flutter/material.dart';
 
 class WebSocketClient {
-  WebSocketChannel? _wsChannel;
+  static WebSocketChannel? _wsChannel;
   final _notificationStream = StreamController<bool>();
 
   final _user = GetIt.I.get<User>();
 
   Stream getStream(){
-    if (_wsChannel == null) {
-      _tryConnectWebsocket();
-    }
-
+    _tryConnectWebsocket();
     return _notificationStream.stream;
   }
 
-  void _tryConnectWebsocket(){
-    debugPrint("TRY CONNECT WEBSOCKET");
+  void _tryConnectWebsocket() async {
+    if (_isDisposed){
+      return;
+    }
+
+    _wsChannel?.sink.close();
     _wsChannel = WebSocketChannel.connect(
       Uri.parse("${CommonUrl.webSocketUrl}/pengajuan/event"),
     );
+    await _wsChannel!.ready;
+
     _wsChannel!.stream.listen(
         (event) {
-
-          _wsChannel!.sink.add(_user.username);
-          // acknowledge
-          _wsChannel!.sink.done
-              .whenComplete(() => _notificationStream.add(true));
-        } ,
+          if (!_isDisposed) {
+            _wsChannel!.sink.add(_user.username);
+            _wsChannel!.sink.done
+              .whenComplete((){
+                if (!_notificationStream.isClosed) {
+                  _notificationStream.add(true);
+                }
+              });
+          }
+        },
         onDone: (){
           Future.delayed(const Duration(seconds: 5) , _tryConnectWebsocket);
-              
         },
         onError: (Object error, StackTrace stackTrace) {
           Future.delayed(const Duration(seconds: 5) , _tryConnectWebsocket);
         }
     );
-    if (_wsChannel!.closeCode == null) {
-      // NGIRIM JWT Token
+    // NGIRIM JWT Token
+    if (!_isDisposed) {
       _wsChannel!.sink.add(_user.token);
     }
   }
 
+  bool _isDisposed = false;
   void dispose(){
-    _wsChannel!.sink.close();
+    _isDisposed = true;
+    _wsChannel?.sink.close();
     _notificationStream.close();
   }
 }
