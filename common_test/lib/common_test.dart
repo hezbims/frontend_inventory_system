c68041495604b1d35b529bf6bf4783c7b12bc3cc
ITP Hezbi Sulaiman <hezbi.sulaiman@idstar.group>
2025-11-25 11:07:10 +0700

    test(common test package) : test utilities yang bisa digunakan di semua package

---
+++ common_test/lib/common_test.dart
@@ -0,0 +1,7 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/mock/mock_time_service.dart';
+export 'src/mock/mock_token_manager.dart';
---
+++ common_test/lib/src/mock/mock_time_service.dart
@@ -0,0 +1,4 @@
+import 'package:common/domain/service/i_time_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockTimeService extends Mock implements ITimeService {}
